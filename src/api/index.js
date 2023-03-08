1b346d999e9ea6f4bba4de5b16bca483db7f4a2f
thefststbllt <bel12pasha@yandex.ru>
2023-03-09 00:27:32 +0300

    added adaptive layout

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import { API_TOKEN, MAX_HOTEL_COUNT } from '../utils/utils';
 
 export const getCityId = async (cityName) => {
-    const request = await fetch(`http://engine.hotellook.com/api/v2/lookup.json?query=${cityName}&lang=ru&lookFor=both&limit=1`)
+    const request = await fetch(`https://engine.hotellook.com/api/v2/lookup.json?query=${cityName}&lang=ru&lookFor=both&limit=1`)
     return await request.json();
 };
 
 export const getCityHotels = async (cityId, checkIn, checkOut) => {
-    const request = await fetch(`http://yasen.hotellook.com/tp/public/widget_location_dump.json?currency=rub&language=ru&limit=${MAX_HOTEL_COUNT}&id=${cityId}&type=popularity&check_in=${checkIn}&check_out=${checkOut}&token=${API_TOKEN}`)
+    const request = await fetch(`https://yasen.hotellook.com/tp/public/widget_location_dump.json?currency=rub&language=ru&limit=${MAX_HOTEL_COUNT}&id=${cityId}&type=popularity&check_in=${checkIn}&check_out=${checkOut}&token=${API_TOKEN}`)
         return await request.json();
 }
